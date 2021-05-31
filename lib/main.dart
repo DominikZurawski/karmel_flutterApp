c05fe338677cfae83abf5bdabbf17a909f7e2598
DominikZurawski <dominikzurawski15@gmail.com>
2021-05-31 21:10:01 +0200

    Calendar add

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:karmel_app/home.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  initializeDateFormatting().then((_) => runApp(MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: [
+      /*localizationsDelegates: [
         GlobalMaterialLocalizations.delegate
       ],
       supportedLocales: [
         const Locale('en'),
         const Locale('pl')
       ],
-      locale : const Locale("pl","PL"),
+      locale : const Locale("pl","PL"),*/
       debugShowCheckedModeBanner: false,
       //home: LandingPage(),
       home: Home(),
