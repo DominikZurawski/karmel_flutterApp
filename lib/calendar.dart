71cfa557dbcd4db77b4665ff67ae5085b440218e
DominikZurawski <dominikzurawski15@gmail.com>
2021-05-31 21:10:01 +0200

    Calendar add

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,80 +1,154 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/widgets.dart';
-import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+import 'package:karmel_app/utils.dart';
 
 
 /// The hove page which hosts the calendar
 class Calendar extends StatefulWidget {
-  /// Creates the home page to display teh calendar widget.
-  /*const MyHomePage(
-      //{Key? key}
-      ) : super(
-    //key: key
-  );
 
-  @override*/
+  @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<Calendar> {
+  ValueNotifier<List<Event>> _selectedEvents;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode
+      .toggledOff; // Can be toggled on/off by longpressing a date
+  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDay;
+  DateTime _rangeStart;
+  DateTime _rangeEnd;
+
+  _MyHomePageState();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedDay = _focusedDay;
+    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay));
+  }
+
+  @override
+  void dispose() {
+    _selectedEvents.dispose();
+    super.dispose();
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    // Implementation example
+    return kEvents[day] ?? [];
+  }
+
+  List<Event> _getEventsForRange(DateTime start, DateTime end) {
+    // Implementation example
+    final days = daysInRange(start, end);
+
+    return [
+      for (final d in days) ..._getEventsForDay(d),
+    ];
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+        _rangeStart = null; // Important to clean those
+        _rangeEnd = null;
+        _rangeSelectionMode = RangeSelectionMode.toggledOff;
+      });
+
+      _selectedEvents.value = _getEventsForDay(selectedDay);
+    }
+  }
+
+  void _onRangeSelected(DateTime start, DateTime end, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = null;
+      _focusedDay = focusedDay;
+      _rangeStart = start;
+      _rangeEnd = end;
+      _rangeSelectionMode = RangeSelectionMode.toggledOn;
+    });
+
+    // `start` or `end` could be null
+    if (start != null && end != null) {
+      _selectedEvents.value = _getEventsForRange(start, end);
+    } else if (start != null) {
+      _selectedEvents.value = _getEventsForDay(start);
+    } else if (end != null) {
+      _selectedEvents.value = _getEventsForDay(end);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Kalendarz"),
-        ),
-
-
-        body: SfDateRangePicker(
-
-          showNavigationArrow: true,
-          view: DateRangePickerView.month,
-
-          headerStyle: DateRangePickerHeaderStyle(
-              backgroundColor: Color(0xff7fcd91),
-              textAlign: TextAlign.center,
-              textStyle: TextStyle(
-                fontStyle: FontStyle.normal,
-                fontSize: 25,
-                //letterSpacing: 5,
-                color: Color(0xffe6efe8),
-              )),
-
-          monthViewSettings: DateRangePickerMonthViewSettings(
-              firstDayOfWeek: 1,
-              blackoutDates: [DateTime(2021, 05, 25)],
-              weekendDays: [6,7],
-              specialDates: [
-                DateTime(2021, 05, 20),
-                DateTime(2021, 05, 16),
-                DateTime(2021, 05, 17)
-              ],
-              showTrailingAndLeadingDates: true,
-              dayFormat: 'EEE'
+      appBar: AppBar(
+        title: Text('TableCalendar - Events'),
+      ),
+      body: Column(
+        children: [
+          TableCalendar<Event>(
+            firstDay: kFirstDay,
+            lastDay: kLastDay,
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            rangeStartDay: _rangeStart,
+            rangeEndDay: _rangeEnd,
+            calendarFormat: _calendarFormat,
+            rangeSelectionMode: _rangeSelectionMode,
+            eventLoader: _getEventsForDay,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            calendarStyle: CalendarStyle(
+              // Use `CalendarStyle` to customize the UI
+              outsideDaysVisible: false,
+            ),
+            onDaySelected: _onDaySelected,
+            onRangeSelected: _onRangeSelected,
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
           ),
-          monthCellStyle: DateRangePickerMonthCellStyle(
-            blackoutDatesDecoration: BoxDecoration(
-                color: Colors.red,
-                border: Border.all(color: const Color(0xFFF44436), width: 1),
-                shape: BoxShape.circle),
-            /*weekendDatesDecoration: BoxDecoration(
-                color: const Color(0xFFDFDFDF),
-                border: Border.all(color: const Color(0xFFB6B6B6), width: 1),
-                shape: BoxShape.circle,
-
-            ),*/
-            weekendTextStyle: TextStyle(color: Colors.red),
-            specialDatesDecoration: BoxDecoration(
-                color: Colors.green,
-                border: Border.all(color: const Color(0xFF2B732F), width: 1),
-                shape: BoxShape.circle),
-            blackoutDateTextStyle: TextStyle(
-                color: Colors.white, decoration: TextDecoration.lineThrough),
-            specialDatesTextStyle: const TextStyle(color: Colors.white),
+          const SizedBox(height: 8.0),
+          Expanded(
+            child: ValueListenableBuilder<List<Event>>(
+              valueListenable: _selectedEvents,
+              builder: (context, value, _) {
+                return ListView.builder(
+                  itemCount: value.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 4.0,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: ListTile(
+                        onTap: () => print('${value[index]}'),
+                        title: Text('${value[index]}'),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
           ),
-        )
+        ],
+      ),
     );
   }
-
 }
