11ae7b9370430736ab6d5239b448ed30e40b2d41
DominikZurawski <dominikzurawski15@gmail.com>
2021-06-11 21:08:14 +0200

    Add Calendar

---
+++ lib/globals.dart
@@ -0,0 +1,8 @@
+library karmel_app.globals;
+//export 'package:karmel_app/globals.dart';
+
+bool isLoggedIn = false;
+DateTime _focusedDay;
+DateTime _selectedDay;
+DateTime _rangeStart;
+DateTime _rangeEnd;
